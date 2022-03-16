d986fd482dd2f3b19aec1e356509082825ae37ad
Phil Hawksworth <phil@netlify.com>
2022-03-16 15:17:36 +0000

    buiding the dev flow for multiple examples

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,16 @@
 const CleanCSS = require("clean-css");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-module.exports = function (eleventyConfig) {
+module.exports = function(eleventyConfig) {
   // pass through static assets
   // ADD YOUR EXAMPLE FOLDERS HERE
   eleventyConfig.addPassthroughCopy({ "src/hello-world/assets": "hello-world" });
+  eleventyConfig.addPassthroughCopy({ "src/edge-includes/assets": "edge-includes" });
   // eleventyConfig.addPassthroughCopy({ "src/MY-NEW-EXAMPLE-FOLDER/assets": "MY-NEW-EXAMPLE-FOLDER" });
 
   eleventyConfig.ignores.add("/src/**/assets/README.md");
 
-  eleventyConfig.addFilter("cssmin", function (code) {
+  eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
   });
 
