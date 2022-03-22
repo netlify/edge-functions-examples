b8fd07ba9ae9744432daee85e3b2b4bfbec1c313
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-03-22 11:20:11 +0000

    Update edge functions info on main layout

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,7 @@
 import head from "./head.js";
 import header from "./header.js";
 import footer from "./footer.js";
-
+import deployButton from "./deploy-button.js";
 
 export default function layout(data) {
   return `
@@ -24,13 +24,22 @@ export default function layout(data) {
     
     <section>
       <h2>What are Edge Functions?</h2>
-      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
-      <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+
+      <p>Netlify Edge Functions connects our powerful web development workflow with an open runtime standard at the network edge, making it easy for developers to build fast, personalized web experiences with existing development tools.<p>
+      
+      <p>Using Javascript and Typescript, Netlify Edge Functions give you the power to modify network requests to localize content, serve relevant ads, authenticate visitors, A/B test content, and much more! And this all happens at the <strong>Edge</strong> — directly from the worldwide location closest to each user.</p>
+
       <a href="/" target="_blank">Explore more examples</a>
-      <h3>Deploy this site to Netlify</h3>
-      <p>lorem ipsum</p>
-      <a href="https://app.netlify.com/start/deploy?repository=https://github.com/netlify/edge-functions-examples">Deploy to Netlify</a>
     </section>
+
+    <hr />
+
+    <section>
+      <h3>Deploy this site to Netlify</h3>
+      <p>Try out Edge Functions on Netlify today! Click the button below to deploy this site to your Netlify account.</p>
+      ${deployButton()}
+    </section>
+
   </main>
   ${footer()}
   </body>
