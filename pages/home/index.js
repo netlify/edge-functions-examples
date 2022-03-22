5f0872b46bcaf41b9b37511624c4a4a3d2cebfae
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-03-22 10:37:13 +0000

    Add page meta system and example open graph images

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,6 @@
 export default function page() {
   return `
-  <h1>Edge Function Examples</h1>
+  <h1>Edge Functions on Netlify</h1>
   <h2>Follow the links for different edge examples</h2>
   <h3>Try these:</h3>
   <ul>
