e2daf0601704e962ed2cd494db63e82212773cef
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-04-04 11:13:03 +0100

    Update home page

--- pages/home/index.js
+++ pages/home/index.js
@@ -4,71 +4,66 @@ export default {
   page: function () {
     return `
     <h1>Edge Functions on Netlify</h1>
-      <p>Explore a library of reference examples for exploring and learning about Edge Functions on Netlify.</p>
+    <p>Explore a library of reference examples for exploring and learning about Edge Functions on Netlify.</p>
 
-      <section class="home__section">
-        <h2 class="home__sectionHeader">Responses</h2>
+    <section class="home__section">
+      <h2 class="home__sectionHeader">Responses</h2>
+      <ul class="home__sectionList">
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/hello">Hello, world</a></li>
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/json">Return JSON</a></li>
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/image">Return an image</a></li>
+      </ul>
+    </section>
 
-        <ul class="home__sectionList">
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/hello">Hello, world</a></li>
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/json">Return JSON</a></li>
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/image">Return an image **TO DO**</a></li>
-        </ul>
-      </section>
+    <section class="home__section">
+      <h2 class="home__sectionHeader">Rewrites and proxies **NOT WORKING**</h2>
+      <ul class="home__sectionList">
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/rewrite">Rewrite a URL</a></li>
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/proxy">Proxy requests to another source **TO DO**</a></li>
+      </ul>
+    </section>
 
-      <section class="home__section">
-        <h2 class="home__sectionHeader">Rewrites and proxies **NOT WORKING**</h2>
+    <section class="home__section">
+      <h2 class="home__sectionHeader">HTTP Headers</h2>
+      <ul class="home__sectionList">
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/set-request-header">Set custom HTTP request headers</a></li>
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/set-response-header">Set custom HTTP response headers</a></li>
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/headers-i18n">Use request headers to serve internationalized content **TO DO**</a></li>
+      </ul>
+    </section>
 
-        <ul class="home__sectionList">
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/rewrite">Rewrite a URL</a></li>
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/proxy">Proxy requests to another source **TO DO**</a></li>
-        </ul>
-      </section>
+    <section class="home__section">
+      <h2 class="home__sectionHeader">Geolocation **NOT WORKING** </h2>
+      <ul class="home__sectionList">
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/geolocation">Determine a user's location</a></li>
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/geoblock">Block content according to country **TO DO**</a></li>
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/internationalization">Serve localized content **TO DO**</a></li>
+      </ul>
+    </section>
 
-      <section class="home__section">
-        <h2 class="home__sectionHeader">HTTP Headers</h2>
-        <ul class="home__sectionList">
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/set-request-header">Set custom HTTP request headers</a></li>
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/set-response-header">Set custom HTTP response headers</a></li>
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/headers-i18n">Use request headers to serve internationalized content **TO DO**</a></li>
-        </ul>
-      </section>
+    <section class="home__section">
+      <h2 class="home__sectionHeader">Cookies</h2>
+      <ul class="home__sectionList">
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/cookies">Set cookies</a></li>
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/cookies">Read cookies</a></li>
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/cookies">Delete cookies</a></li>
+      </ul>
+    </section>
 
-      <section class="home__section">
-        <h2 class="home__sectionHeader">Geolocation **NOT WORKING** </h2>
-        <ul class="home__sectionList">
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/geolocation">Determine a user's location</a></li>
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/geoblock">Block content according to country **TO DO**</a></li>
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/internationalization">Serve localized content **TO DO**</a></li>
-        </ul>
-      </section>
+    <section class="home__section">
+      <h2 class="home__sectionHeader">A/B tests</h2>
+      <ul class="home__sectionList">
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/abtest">Set up an A/B test using cookies</a></li>
+      </ul>
+    </section>
 
-      <section class="home__section">
-        <h2 class="home__sectionHeader">Cookies</h2>
-
-        <ul class="home__sectionList">
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/cookies">Set cookies</a></li>
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/cookies">Read cookies</a></li>
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/cookies">Delete cookies</a></li>
-        </ul>
-      </section>
-
-      <section class="home__section">
-        <h2 class="home__sectionHeader">A/B tests</h2>
-
-        <ul class="home__sectionList">
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/abtest">Set up an A/B test using cookies</a></li>
-        </ul>
-      </section>
-
-      <section class="home__section">
-        <h2 class="home__sectionHeader">Transforming responses</h2>
-
-        <ul class="home__sectionList">
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/transform">Text transformations</a></li>
-          <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/include">Content includes</a></li>
-        </ul>
-      </section>
+    <section class="home__section">
+      <h2 class="home__sectionHeader">Transforming responses</h2>
+      <ul class="home__sectionList">
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/transform">Text transformations</a></li>
+        <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/include">Content includes</a></li>
+      </ul>
+    </section>
   `;
   },
 };
