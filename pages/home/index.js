5a9f92021564d29dcca6e7171f671ffbbbf6aeee
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2022-04-04 11:23:00 +0100

    Move a/b test example to cookies area

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,7 +1,7 @@
 export default {
   title: "Home",
   metaDescription: "Explore our library of Edge Function examples and deploy your own to Netlify.",
-  page: function() {
+  page: function () {
     return `
     <h1>Edge Functions on Netlify</h1>
     <p>Explore a library of reference examples for exploring and learning about Edge Functions on Netlify.</p>
@@ -41,12 +41,6 @@ export default {
         <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/cookies-set">Set cookies</a></li>
         <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/cookies-read">Read cookies</a></li>
         <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/cookies-delete">Delete cookies</a></li>
-      </ul>
-    </section>
-
-    <section class="home__section">
-      <h2 class="home__sectionHeader">A/B tests</h2>
-      <ul class="home__sectionList">
         <li class="home__sectionListItem"><a class="home__sectionListItemLink" href="/example/abtest">Set up an A/B test using cookies</a></li>
       </ul>
     </section>
