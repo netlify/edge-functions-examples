049c170929c576c1ad0f56646e5b8f420ade0bd3
@whitep4nth3r <52798353+whitep4nth3r@users.noreply.github.com>
2022-04-26 11:30:50 +0100

    Add UTM tracking to outbound links

--- components/deploy-button.js
+++ components/deploy-button.js
@@ -1,6 +1,6 @@
 export default function deployButton() {
   return `
-  <a href="https://app.netlify.com/start/deploy?repository=https://github.com/netlify/edge-functions-examples" rel="nofollow" class="cta-button">
+  <a href="https://app.netlify.com/start/deploy?repository=https://github.com/netlify/edge-functions-examples&utm_campaign=devex&utm_source=edge-functions-examples&utm_medium=web&utm_content=Deploy%20Edge%20Functions%20Examples%20to%20Netlify" target="_blank" rel="noopener" class="cta-button">
     Deploy to Netlify
   </a>
   `;
--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,8 @@ export default function footer() {
   const year = date.getFullYear();
   return `
     <footer class="footer">
-      <p class="footer__copyright">© ${year} Netlify</p>
+      <p class="footer__copyright">© ${year} <a href="https://netlify.com/?utm_campaign=devex&utm_source=edge-functions-examples&utm_medium=web&utm_content=Netlify%20Home" target="_blank" rel="noopener">Netlify</a></p>
+      <p class="footer__source"><a href="https://github.com/netlify/edge-functions-examples/" target="_blank" rel="noopener">View source code on GitHub</a></p>
     </footer>
   `;
 }
