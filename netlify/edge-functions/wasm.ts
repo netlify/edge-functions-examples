b1daf0ab95c88da2163d9264a08219a3a95239df
Simon Knott <info@simonknott.de>
2023-06-26 11:36:11 +0200

    Load .wasm file from dir

--- netlify/edge-functions/wasm.ts
+++ netlify/edge-functions/wasm.ts
@@ -1,17 +1,9 @@
-// inlined Rust compiled code that prints "42"
-const wasmCode = new Uint8Array([
-  0,97,115,109,1,0,0,0,1,133,128,128,128,0,1,96,0,1,127,
-  3,130,128,128,128,0,1,0,4,132,128,128,128,0,1,112,0,0,
-  5,131,128,128,128,0,1,0,1,6,129,128,128,128,0,0,7,145,
-  128,128,128,0,2,6,109,101,109,111,114,121,2,0,4,109,97,
-  105,110,0,0,10,138,128,128,128,0,1,132,128,128,128,0,0,
-  65,42,11
-]);
+// Rust compiled code that prints "42"
+const { instance } = await WebAssembly.instantiateStreaming(
+  fetch(import.meta.resolve("./fourty-two.wasm"))
+);
+const main = instance.exports.main as CallableFunction;
 
-const wasmModule = new WebAssembly.Module(wasmCode);
-const wasmInstance = new WebAssembly.Instance(wasmModule);
-const main = wasmInstance.exports.main as CallableFunction;
-
-export default async () => {
+export default () => {
   return new Response(`The answer is ${main().toString()}`);
-}
+};
