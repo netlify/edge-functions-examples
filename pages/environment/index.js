1b64ebff9b2bf0142c395d30cfebc9879a49dd67
Ivan <ivan.zarea@netlify.com>
2023-09-06 11:25:48 +0200

    Use Netlify.env instead of Deno.env

--- pages/environment/index.js
+++ pages/environment/index.js
@@ -7,12 +7,12 @@ export default {
     return `
     <section>
       <h1>Use environment variables</h1>
-      <p>Netlify Edge Functions support open-source Deno APIs. To access your Netlify environment variables in Edge Functions, use the <code>Deno.env</code> API.</p> 
+      <p>To access your Netlify environment variables in Edge Functions, use the <code>Netlify.env</code> API.</p>
 
       <pre><code>import type { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
-  const value = Deno.env.get("MY_IMPORTANT_VARIABLE");
+  const value = Netlify.env.get("MY_IMPORTANT_VARIABLE");
 
   return new Response(&grave;Value of MY_IMPORTANT_VARIABLE is "&dollar;{value}".&grave;, {
     headers: { "content-type": "text/html" },
@@ -21,7 +21,7 @@ export default async (request: Request, context: Context) => {
       <h2>See this in action</h2>
       <ul>
         <li><a href="/environment">View the response from the Edge Function</a></li>
-        <li><a href="https://doc.deno.land/deno/stable/~/Deno.env" target="_blank" rel="noopener">View the Deno.env API docs</a></li>
+        <li><a href="https://docs.netlify.com/edge-functions/api/#netlify-global-object" target="_blank" rel="noopener">View the Netlify.env API docs</a></li>
         <li>${repoLink("environment.ts")}</li>
       </ul>
     </section>
